9d25d2cbc86ae34e99cfa7b8f9f7fe72a17563a1
Wkunal <Kunalwakode001@gmail.com>
2026-03-31 15:58:16 +0530

    Test

--- Mango.Services.ProductAPI/obj/Debug/net8.0/Mango.Services.ProductAPI.AssemblyInfo.cs
+++ Mango.Services.ProductAPI/obj/Debug/net8.0/Mango.Services.ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7667f89fdf4b3429b33d13da6d4685685549f6bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
